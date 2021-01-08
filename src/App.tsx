36f0b750041781002f7cf6a9aeccad84d177b2d2
황연욱 <yeonuk@hwang-yeon-ug-ui-MacBookPro-2.local>
2021-01-08 16:17:55 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const hi = 'huhiu';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
